f33433d63c2f916bbd5be13f61559e36e1722697
abishek <abishek004m@gmail.com>
2025-10-28 20:34:49 +0530

    intial commit

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.onscroll = () => {
     });
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky',window.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
 
     menuIcon.classList.remove('bx-x');
@@ -35,12 +35,6 @@ window.onscroll = () => {
 
 };
 
-ScrollReveal({
-    // reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-})
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'bottom' });
